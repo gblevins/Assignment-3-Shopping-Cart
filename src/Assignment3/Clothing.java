c9f931314303e4dec5c3df7b419b349060abc3f8
malvikagupta <mg42972@bme-lrc-854707.austin.utexas.edu>
2016-02-23 21:42:08 -0600

    test
    checking if my changes are being saved

--- src/Assignment3/Clothing.java
+++ src/Assignment3/Clothing.java
@@ -4,6 +4,7 @@ public class Clothing extends Item
 {
 // same as Item class
 // constructor
+	
 	public Clothing(String itemName, float itemPrice, int itemQuantity, int itemWeight)
 	{
 		name = itemName;
